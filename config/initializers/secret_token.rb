10bd94432aae62a6bac7e67d016baedb1c2b4a81
Mao Pechenda <maolongdy@ubuntu.ubuntu-domain>
2013-02-14 07:25:41 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LongdyApp::Application.config.secret_token = 'b05a126eb795a587a6dbc028ee2df58a3b099d4d9c7474104a9f2601b59c152e45717e900dd8514e227247f101ce6bac48ed43eee10afe65627636858d1b9ebd'
